39f170fb7aaa501b30ec1d99a6dcd040a09aeabc
Isaac Ikenna Magnus <isaacikennamagnus@Isaacs-MacBook-Air.local>
2022-06-25 09:59:31 +0200

    First Push on Javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+/*This is a javascript code that prints out myName, myHeight, and myCountry */
+
+console.log('Isaac Ikenna Magnus');
+console.log('Height: 182cm');
+console.log('Country: Nigeria');
