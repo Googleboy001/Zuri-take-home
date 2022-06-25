8b7f1e02ddce321c949e34c2910dce34b80c2f68
Isaac Ikenna Magnus <68070091+Googleboy001@users.noreply.github.com>
2022-06-25 12:02:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-/*This is a javascript code that prints out myName, myHeight, and myCountry */
 
-const name= "Isaac Ikenna Magnus";
-const height= "182cm";
-const country= "Nigeria";
-
-
-alert("\n NAME: " + name + " \n \n \n" + "HEIGHT: " + height + "\n \n \n" + "COUNTRY: " + country);
-console.log(name + " \n \n" + height +"\n \n + country);
